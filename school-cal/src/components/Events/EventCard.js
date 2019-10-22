f020f302b80b3f2825cd9f50d5119456e81854bf
Landry Irakoze <irakozel@gmail.com>
2019-10-22 13:51:56 -0400

    connect to firebase

--- school-cal/src/components/Events/EventCard.js
+++ school-cal/src/components/Events/EventCard.js
@@ -4,6 +4,7 @@ const EventCard = (props) => {
 
     return (
         <>
+        
         </>
     )
 }
--- school-cal/src/components/Events/EventDisplay.js
+++ school-cal/src/components/Events/EventDisplay.js
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import EventCard from './EventCard';
+import firebase, { db } from '../../firebase/'
+// import EventCard from './EventCard';
+import './EventDisplay.css'
 
 const EventDisplay = () => {
 
     useEffect(() => {
-
-
+        db
+            .collection("calendars")
+            .doc("aWBlKnc7wrTCOQenDfaA")
+            .collection("events")
+            .get()
+            .then(snapshot => {
+                snapshot.docs.map(doc => console.log(doc.data()))
+            })
     },[])
 
-    const [events, setEvents] = useState([]);
+    // const [events, setEvents] = useState([]);
+    // const async snapshot = await firebase.firebase().collection('calendars').doc('aWBlKnc7wrTCOQenDfaA').collection('events').get()
+    // console.log('this is the snapshop', snapshot)
 
+    // return snapshot.docs.map(doc => doc.data())
     return (
-        <>
-        {events.map(item => <EventCard info={item}/>)}
-        </>
+        <div className='event-display-container'>
+            <h1>Events Go HERE</h1>
+
+        </div>
     )
 }
 
