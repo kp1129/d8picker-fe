139c175b23c7e857552418deca8de0720d9fb360
Landry Irakoze <irakozel@gmail.com>
2019-10-23 14:33:36 -0400

    sanity commit

--- school-cal/src/components/Events/EventDisplay.js
+++ school-cal/src/components/Events/EventDisplay.js
@@ -8,7 +8,7 @@ const EventDisplay = () => {
 
     const eventsDbRef = db
         .collection("calendars")
-        .doc("U3pUUyW2xKV0nleepKrP")
+        .doc("2ErFGeyXi7p7yYeOaOvH")
         .collection("events");
     //keep in state first calendar as default, have dropdown select for individual calendars
 
