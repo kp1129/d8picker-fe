a4d8c23dd61ca1b596275335f792fc1c90c9b133
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2019-10-24 13:36:13 -0700

    added in basic cards that display events

--- school-cal/src/components/Events/EventDisplay.js
+++ school-cal/src/components/Events/EventDisplay.js
@@ -3,31 +3,33 @@ import firebase, { db } from '../../config/firebase/index'
 // import EventCard from './EventCard';
 import './EventDisplay.css'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 const EventDisplay = () => {
+    const classes = useStyles();
     const [data, setData] = useState([]);
-    const [calendars, setCalendars] = useState([]);
-    const [admins, setAdmins] = useState([]);
-
-    const userID = firebase.auth().currentUser.uid;
-
-    const calDbRef = db
-        .collection("calendars")
-        .where("admins", "array-contains", userID);
-
-    useEffect(() => {
-        calDbRef
-            .onSnapshot(doc => {
-                doc.docs.map(item => console.log('admins', item.data()))
-                setAdmins(doc.docs.map(item => item.data()))
-            });
-    })
-    // calDbRef
-    //     .get()
-    //     .then(snapshot => {
-    //         snapshot.docs.map(doc => console.log('primarycal', doc.data()))
-    //     })
-
-    console.log('primarycal', calDbRef);
 
     const eventsDbRef = db
         .collection("calendars")
@@ -37,10 +39,12 @@ const EventDisplay = () => {
 
     useEffect(() => {
         eventsDbRef
-            .onSnapshot(doc => {
-                doc.docs.map(item => console.log('events', item.data()))
-                setData(doc.docs.map(item => item.data()))
+            .get()
+            .then(snapshot => {
+                snapshot.docs.map(doc => console.log(doc.id))
+                setData(snapshot.docs.map(doc => doc.data()))
             })
+            .catch(err=> console.log('something is up', err))
     },[]) //have an empty array, do .map then push to empty array, set data to array
 
     // useEffect(() => {
@@ -63,28 +67,30 @@ const EventDisplay = () => {
 
         <div className='event-display-container'>
             <h1>Events Go HERE</h1>
-            <ul 
-                className='event-display-cards'
-                >
-                {data.map(item => (
-                    <li
-                        className='event-display-card'
-                        key={item.date}
-                    >
-                        {/* <br />
-                        When: {item.date} */}
-                        <br />
-                        description: {item.description}
-                        <br />
-                        Location: {item.location}
-                        <br />
-                        Name: {item.name}
-                        {/* <br />
-                        User ID: {item.uid}
-                        <button>X</button> */}
-                    </li>
-                ))}
-            </ul>
+            <div style={{display:'flex', margin:"2%"}}>
+                {data.map(info => { return (
+                    <Card className={classes.card} style={{width:"20%", margin:"2%"}}>
+                    <CardContent>
+                      <Typography   gutterBottom>
+                        Event: {info.name}
+                      </Typography>
+                      <Typography>
+                       Location: {info.location}
+                      </Typography>
+
+                      <Typography variant="body2" component="p">
+                        Description: {info.description}
+                      </Typography>
+                      <Typography variant="body2" component="p">
+                        Time: {info.time}
+                      </Typography>
+                    </CardContent>
+
+                  </Card>
+                )}
+
+                )}
+            </div>
 
         </div>
     )
