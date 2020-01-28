d3e68e68db2a46589067377abfd65fe0776fb8c1
DeAndre Johnson <dre@phreshr.com>
2020-01-29 07:25:03 +0800

    use useAuth login handler

--- src/components/Splash/LoginBar.js
+++ src/components/Splash/LoginBar.js
@@ -1,99 +1,93 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-
-import axios from 'axios';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { TextField, Button, makeStyles } from "@material-ui/core";
 
 // Pull in Modal component (register form)
-import RegisterModal from './RegisterModal';
-
-import { TextField, Button, makeStyles } from '@material-ui/core';
-
-import '../../App.css';
+import RegisterModal from "./RegisterModal";
+import { useAuth } from "../../hooks/useAuth";
+import "../../App.css";
 
 // Styling for material-ui
 const useStyles = makeStyles(theme => ({
-	root: {
-		flexGrow: 1
-	},
-	form: {
-		display: 'flex',
-		margin: '0 3%'
-	},
-	input: {
-		background: 'white',
-		margin: 5,
-		borderRadius: 5
-	},
-	button: {
-		background: '#caad0fd4',
-		fontSize: '.8rem',
-		padding: 10,
-		width: '10%'
-	}
+  root: {
+    flexGrow: 1
+  },
+  form: {
+    display: "flex",
+    margin: "0 3%"
+  },
+  input: {
+    background: "white",
+    margin: 5,
+    borderRadius: 5
+  },
+  button: {
+    background: "#caad0fd4",
+    fontSize: ".8rem",
+    padding: 10,
+    width: "10%"
+  }
 }));
 
 function Login() {
-	const classes = useStyles();
-	const history = useHistory();
-	const [user, setUser] = useState({
-		email: '',
-		password: ''
-	});
+  const classes = useStyles();
+  const history = useHistory();
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+  const { handleLogin } = useAuth();
 
-	const handleChanges = e => {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	};
+  const handleChanges = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		console.log('Login', user);
-		axios
-			.post('https://d8picker.herokuapp.com/api/user/login', user)
-			.then(res => {
-				console.log('Post', res);
-				localStorage.setItem('token', res.data.token);
-				history.push('/home');
-			})
-			.catch(err => {
-				console.log(err);
-			});
-		setUser({ email: '', password: '' });
-	};
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log("Login", user);
+    try {
+      await handleLogin(user);
+      setUser({ email: "", password: "" });
+      history.push("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<div className='navbar'>
-			<RegisterModal />
-			<form onSubmit={handleSubmit} className={classes.form}>
-				<TextField
-					type='email'
-					id='outlined-basic'
-					label='email'
-					name='email'
-					margin='normal'
-					variant='outlined'
-					value={user.email}
-					onChange={handleChanges}
-					className={classes.input}
-					required
-				/>
-				<TextField
-					type='password'
-					id='outlined-basic'
-					label='password'
-					name='password'
-					margin='normal'
-					variant='outlined'
-					value={user.password}
-					onChange={handleChanges}
-					className={classes.input}
-					required
-				/>
-				<Button type='submit' className={classes.button}>
-					sign in
-				</Button>
-			</form>
-		</div>
-	);
+  return (
+    <div className="navbar">
+      <RegisterModal />
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <TextField
+          type="email"
+          id="outlined-basic"
+          label="email"
+          name="email"
+          margin="normal"
+          variant="outlined"
+          value={user.email}
+          onChange={handleChanges}
+          className={classes.input}
+          required
+        />
+        <TextField
+          type="password"
+          id="outlined-basic"
+          label="password"
+          name="password"
+          margin="normal"
+          variant="outlined"
+          value={user.password}
+          onChange={handleChanges}
+          className={classes.input}
+          required
+        />
+        <Button type="submit" className={classes.button}>
+          sign in
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
