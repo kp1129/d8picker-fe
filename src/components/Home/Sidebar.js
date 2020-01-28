1971d55a6bbaa8ae2d8be450b008f0705b1d8027
Daniel <danstad2012@gmail.com>
2020-01-28 10:55:33 -0800

    added material UI to username

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Typography, Paper } from '@material-ui/core'
+import { Typography, Paper, makeStyles } from '@material-ui/core'
 
+
+const styles = makeStyles(theme => ({
+  paper: {
+    background: '#caad0fd4'
+  }
+}))
 
 const Sidebar = () => {
+  const classes = styles()
   const [user, setUser] = useState('Bob')
 
   useEffect(() => {
@@ -17,7 +24,7 @@ const Sidebar = () => {
 
   return (
     <div>
-      <Paper>
+      <Paper className={{classes.paper}}>
         <Typography variant='h1'>
           {user.username}
         </Typography>
