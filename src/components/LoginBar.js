51193ff90831378f8701a163d85911479edb2180
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-28 13:15:32 -0800

    Add post urls for register and login. Test on browser.

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -2,58 +2,63 @@ import React, { useState } from 'react';
 
 import axios from 'axios';
 
+// Pull in Modal component (register form)
 import Modal from './Modal'
 
 import { TextField, Button, makeStyles } from '@material-ui/core';
 
 import '../App.css';
 
+// Styling for material-ui
 const useStyles = makeStyles(theme => ({
 	root: {
 		flexGrow: 1
 	},
 	form: {
 		display: 'flex',
-        width: '60%'
+        margin: '0 3%',
 	},
 	input: {
 		background: 'white',
-		margin: 5,
+        margin: 5,
 		borderRadius: 5
 	},
 	button: {
 		background: '#caad0fd4',
-        fontSize: '1rem',
-        padding: 10
+        fontSize: '.8rem',
+        padding: 10,
+        width: '10%'
     },
 
 }));
+
 
 function Login() {
 	const classes = useStyles();
 
 	const [user, setUser] = useState({
-		username: '',
+		email: '',
 		password: ''
 	});
 
 	const handleChanges = e => {
         setUser({ ...user, [e.target.name]: e.target.value });
-       
 	};
 
 	const handleSubmit = e => {
 		e.preventDefault();
 		console.log('Login',user);
 		axios
-			.post('', user)
+			.post('https://d8picker.herokuapp.com/api/user/login', user)
 			.then(res => {
-				console.log('Post', res);
+                console.log('Post', res);
+                localStorage.setItem('token', res.data.token)
 			})
 			.catch(err => {
 				console.log(err);
 			});
-		setUser({ username: '', password: '' });
+        setUser({ email: '', password: '' });
+        
 	};
 
 	return (
@@ -63,11 +68,11 @@ function Login() {
 				<form onSubmit={handleSubmit} className={classes.form}>
 					<TextField
 						id='outlined-basic'
-						label='username'
-						name='username'
+						label='email'
+						name='email'
 						margin='normal'
 						variant='outlined'
-						value={user.username}
+						value={user.email}
 						onChange={handleChanges}
 						className={classes.input}
 						required
