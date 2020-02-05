354c2fde7f00563c4115e4be4ce01407b2ceca04
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-05 14:44:28 -0800

    Create more boilerplate for calendar.

--- src/components/Home/Calendar.js
+++ src/components/Home/Calendar.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import dateFns from 'date-fns'
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils'
 
 function Calendar() {
+    const [ cal , setCal] = useState({
+        currentMonth: new Date(),
+        selectedDate: new Date ()
+    })
+
+    renderHeader() {}
+
+    renderDays() {}
+
+    renderCells() {}
+
+    onDateClick = day => {}
+
+    nextMonth = () => {}
+
+    prevMonth = () => {
+
+    }
     return (
-        <div>
-            <div>Header</div>
-            <div>Days</div>
-            <div>Cells</div>
+        <div className="calendar">
+            {renderHeader()}
+            {renderDays()}
+            {renderCells()}
         </div>
     )
 }
