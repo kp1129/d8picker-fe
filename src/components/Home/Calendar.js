56a63715bef327fab2aaf5f9fe0f5d4b3b9d2f80
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-05 14:48:48 -0800

    Create logic for renderHeader.

--- src/components/Home/Calendar.js
+++ src/components/Home/Calendar.js
@@ -8,7 +8,27 @@ function Calendar() {
         selectedDate: new Date ()
     })
 
-    renderHeader() {}
+    renderHeader() {
+        const dateFormat = 'MMMM YYYY';
+
+        return(
+            <div className="header row flex-middle">
+      <div className="col col-start">
+        <div className="icon" onClick={this.prevMonth}>
+          chevron_left
+        </div>
+      </div>
+      <div className="col col-center">
+        <span>
+          {dateFns.format(this.state.currentMonth, dateFormat)}
+        </span>
+      </div>
+      <div className="col col-end" onClick={this.nextMonth}>
+        <div className="icon">chevron_right</div>
+      </div>
+    </div>
+        );
+    }
 
     renderDays() {}
 
