558d2bc876e4241b4d62db747ffa4e94e3d5b401
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-07 13:38:03 -0800

    Develop button in Login component.

--- src/components/Splash/LoginBar.js
+++ src/components/Splash/LoginBar.js
@@ -4,9 +4,10 @@ import '../../App.css';
 
 
 function Login() {
+	
 	return (
 		<div className='navbar'>
-			<div ><a  href='http://localhost:4000/api/auth/google'><img src={GoogleBtn} /></a></div>
+			<div ><a href='http://localhost:4000/api/auth/google'><img src={GoogleBtn} alt=''/></a></div>
 			
 		</div>
 	);
