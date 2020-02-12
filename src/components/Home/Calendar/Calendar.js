ce9097c305dc9ed5fd09eee1dda4ae3d77d35169
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-12 13:52:55 -0800

    Pull in currentDay. Declare weekdays.

--- src/components/Home/Calendar/Calendar.js
+++ src/components/Home/Calendar/Calendar.js
@@ -3,6 +3,9 @@ import dayjs from "dayjs";
 import axios from "axios";
 import "./style.css";
 
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const currentDay = dayjs();
+
 const Calendar = props => {
   
   
