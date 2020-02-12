c65c25d2d1c25c68b6e66b6d88f1788069cbdfbf
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-12 13:50:02 -0800

    Refacto to boilerplate.
    coauthor:drejohnson

--- src/components/Home/Calendar/Calendar.js
+++ src/components/Home/Calendar/Calendar.js
@@ -1,17 +1,12 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import DaysOfWeek from "./DaysOfWeek";
-import Cells from './Cells'
-import "./Calendar.css";
+import React, { useState, useEffect } from "react";
+
 
 const Calendar = props => {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  
   
   return (
     <div className="calendar">
-      <Header setCurrDate = {setCurrentDate} currDate = {currentDate} />
-      <DaysOfWeek currDate = {currentDate}/>
-      <Cells currDate = {currentDate} />
+     
     </div>
   );
 };
