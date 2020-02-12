e9bb15a2e9314f2d23cba2283f50da828916ab1e
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-12 13:54:23 -0800

    Create state and logic for calendar.
    coauthor:drejohnson

--- src/components/Home/Calendar/Calendar.js
+++ src/components/Home/Calendar/Calendar.js
@@ -7,7 +7,20 @@ const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const currentDay = dayjs();
 
 const Calendar = props => {
-  
+  const [events, setEvents] = useState(mockEvents.events);
+  const [date, setDate] = useState(dayjs());
+
+  const currentYear = date.year();
+  const currentMonth = date.month(); // January = 0
+  const daysInMonth = date.daysInMonth();
+
+  const firstDayOfMonth = dayjs(`${currentYear}-${currentMonth + 1}-1`);
+  const weekDayOfFirstDay = firstDayOfMonth.day(); // Sunday = 0
+
+  const lastDayOfMonth = dayjs(
+    `${currentYear}-${currentMonth + 1}-${daysInMonth}`
+  );
+  const weekDayOfLastDay = lastDayOfMonth.day();
   
   return (
     <div className="calendar">
