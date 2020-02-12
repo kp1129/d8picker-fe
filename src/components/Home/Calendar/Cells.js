0bd44cf8a78b36edab29b2ac761b5d91024a7960
Andrew <andrewryanogle@gmail.com>
2020-02-12 10:14:56 -0800

    populating calendar

--- src/components/Home/Calendar/Cells.js
+++ src/components/Home/Calendar/Cells.js
@@ -12,10 +12,15 @@ import {
   addDays
 } from "date-fns";
 
+import Loading from '../loadingScreen/loading.js'
+import moment from 'moment'
+
 const Cells = props => {
   const [data, setData] = useState({events:[{}]});
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const toggle = () => setModal(!modal);
   const monthStart = startOfMonth(props.currDate);
   const monthEnd = endOfMonth(monthStart);
@@ -34,19 +39,25 @@ const Cells = props => {
       localStorage.setItem("googleId:", res.data.googleId)
       console.log("results: ", results);
       setData(results);
+      setLoading(true);
     })();
   }, [setData]);
   console.log("andrew data:",data)
   const onDateClick = day => {
     setSelectedDate(day);
   };
+  if(loading){
   while (day <= endDate) {
     for (let i = 0; i < 7; i++) {
       formattedDate = format(day, dateFormat);
-      let eventDate = format(day, 'yyyymmdd')
+      let eventDate = moment(day).format('YYYYMMDD')
       const cloneDay = day;
       let eventDescription = data.events[i]
+      let eventData = data.events[i - 1].start.dateTime
       console.log('single events',data.events[i])
+      console.log("formated date :", moment(eventData).format('YYYYMMDD'))
+      console.log("regular day :", eventDate)
+      
       days.push(
         <div
           className={`column cell ${
@@ -63,7 +74,7 @@ const Cells = props => {
           }}
         >
         {}
-          
+          <span className = "events">{eventDate === moment(eventData).format('YYYYMMDD')? data.events[i].description : null}</span>
           <span className="number">{formattedDate}</span>
           <span className="bg">{formattedDate}</span>
         </div>
@@ -78,7 +89,8 @@ const Cells = props => {
     );
     days = [];
   }
-  return <div className="body">{rows}
+}
+  return <div className="body">{!loading ? <Loading/> : rows}
   {/* <ul>
           {data &&
             data.events.map(event => (
@@ -90,6 +102,7 @@ const Cells = props => {
               </li>
             ))}
         </ul> */}
+        
   </div>;
 };
 export default Cells;
