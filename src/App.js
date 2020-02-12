26b70bd7bba8556240e10a5e8ed77ae3a915a95e
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-12 14:52:16 -0800

    Fix bugs to display calendar.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Splash from './components/Splash/';
 import Home from './components/Home/';
 // import PrivateRoute from "./components/PrivateRoute";
 import Redirect from './components/Redirect';
-import Events from './components/Home/Events';
+
 
 
 import './App.css';
@@ -20,10 +20,6 @@ function App() {
 			{/* <PrivateRoute path="/home">
         <Home />
       </PrivateRoute> */}
-			
-			<Route path='/events'>
-				<Events />
-			</Route>
 			<Route path='/redirect'>
 				<Redirect />
 			</Route>
