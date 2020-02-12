0c705ae26029fb5cae3f684eb90e04156e7a412a
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-12 14:03:39 -0800

    Remove unneeded code.
    coauth:drejohnson

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import Calendar from './Calendar/Calendar';
-import Sidebar from './Sidebar';
-// import GoogleFormConnect from './GoogleFormConnect';
 
 import NavBar from './Navbar';
 
 import '../../App.css';
-// import AddEventForm from './AddEventForm'
 
-// const style = {
-// 	border: 0,
-// 	width: '900px',
-// 	height: '75vh',
-// 	frameborder: '0',
-// 	scrolling: 'no'
-// };
 
 const Home = () => {
-	// const [email, setEmail] = useState('')
-
-
-	// useEffect(() => { setEmail('funnyusernamego') }, []);
-
-	// const cal = `https://calendar.google.com/calendar/embed?src=${email}%40gmail.com&ctz=America%2FLos_Angeles`
-  
-	// 	const [cal, setCal] = useState(
-	// 		'https://calendar.google.com/calendar/embed?src=funnyusernamego%40gmail.com&ctz=America%2FLos_Angeles'
-	// 	);
-
-	// 	useEffect(() => {
-	// 		setCal(
-	// 			'https://calendar.google.com/calendar/embed?src=funnyusernamego%40gmail.com&ctz=America%2FLos_Angeles'
-	// 		);
-	//   }, []);
+	
 
 	return (
 		<div>
@@ -49,9 +24,7 @@ const Home = () => {
 			<main>
 				<Calendar />
 			</main>
-			{/* <GoogleFormConnect /> */}
-			{/* <AddEventForm /> */}
-			{/* <iframe src={cal} style={style}></iframe> */}
+			
 		</div>
 	);
 };
