dca001defeda67317c6011d5a8cfa475a7567391
DeAndre Johnson <dre@phreshr.com>
2020-02-21 05:03:18 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Splash from './components/Splash/';
-import Home from './components/Home/';
+import Splash from "./components/Splash/";
+import Home from "./components/Home/";
 // import PrivateRoute from "./components/PrivateRoute";
-import Redirect from './components/Redirect';
+import Redirect from "./components/Redirect";
+import Authenticate from "./components/Authenticate";
+import Events from "./components/Events";
 
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
-	return (
-		<div className='App'>
-			<Route exact path='/'>
-				<Splash />
-			</Route>
-			{/* Remember to change back to private */}
-			{/* <PrivateRoute path="/home">
+  return (
+    <div className="App">
+      <Route exact path="/">
+        <Splash />
+      </Route>
+      {/* Remember to change back to private */}
+      {/* <PrivateRoute path="/home">
         <Home />
       </PrivateRoute> */}
-			<Route path='/redirect'>
-				<Redirect />
-			</Route>
-			<Route path='/home'>
-				<Home />
-			</Route>
-		</div>
-	);
+      <Route path="/authenticate/google">
+        <Authenticate />
+      </Route>
+      <Route path="/redirect">
+        <Redirect />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/events">
+        <Events />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
