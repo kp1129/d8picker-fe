f2bc2d5bebceb16f07f8aade0af31fe39d0ae7f1
juarezfrench <markedfrench@gmail.com>
2020-02-24 17:23:25 -0600

    Changes to spec files and uninstalled Jest

--- src/components/Authenticate.spec.js
+++ src/components/Authenticate.spec.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Authenticate from './Authenticate';
+import {shallow} from 'enzyme';
+import Authenticate from './Authenticate'
 
 describe('Authenticate', () => {
   it('should render correctly', () => {
     const component = shallow(<Authenticate />);
-
+  
     expect(component).toMatchSnapshot();
   });
+
+  
 });
+
