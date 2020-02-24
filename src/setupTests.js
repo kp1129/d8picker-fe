fe8f9bf3aa7bbdee1a9f5a2f7daea57f40585357
juarezfrench <markedfrench@gmail.com>
2020-02-24 09:38:24 -0600

    Comment out test setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,11 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// configure({ adapter: new Adapter() });
 
-const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    clear: jest.fn(),
-  };
-  global.localStorage = localStorageMock;
+// const localStorageMock = {
+//     getItem: jest.fn(),
+//     setItem: jest.fn(),
+//     removeItem: jest.fn(),
+//     clear: jest.fn(),
+//   };
+//   global.localStorage = localStorageMock;
