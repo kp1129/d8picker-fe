85e7c46cf9656cbef1623c9e8f80390d3e55ebf9
DeAndre Johnson <dre@phreshr.com>
2020-03-04 01:09:21 +0800

    use new routes and styling

--- src/App.js
+++ src/App.js
@@ -1,84 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 // import ReactGA from 'react-ga';
-import Splash from './components/Splash/';
-import Home from './components/Home/';
+import { Stack, Flex } from '@chakra-ui/core';
+import Authenticate from './routes/Authenticate';
+import { useAuth } from './contexts/auth';
+import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import Authenticate from './components/Authenticate';
-import Events from './components/Events';
-import useGapi from './hooks/useGapi';
-import calendarApi from './gapi/calendarApi';
-
-import './App.css';
+import Welcome from './routes/Welcome';
+import Dashboard from './routes/Dashboard';
 
 function App() {
-  const [eventsApi, setEventsApi] = useState();
-
-  const onGapiLoaded = gapiClient => setEventsApi(calendarApi(gapiClient));
+  const { googleApi } = useAuth();
 
   // function initializeAnalytics() {
   //   ReactGA.initialize('UA-157827018-1');
   //   ReactGA.pageview('/home');
   // }
 
-  const googleApi = useGapi({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    scope: 'https://www.googleapis.com/auth/calendar.events',
-    discoveryDocs: [
-      'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
-    ],
-    ux_mode: 'redirect',
-    redirect_uri: `${process.env.REACT_APP_BASE_URL}/authenticate/google`,
-    onLoaded: onGapiLoaded
-  });
-
-  const {
-    isLoading,
-    isAuthenticated,
-    currentUser,
-    onSignIn,
-    onSignOut
-  } = googleApi;
-
-  useEffect(() => {
-    isAuthenticated
-      ? localStorage.setItem('isAuthenticated', true)
-      : localStorage.removeItem('isAuthenticated');
-  }, [isAuthenticated]);
-
-  if (isLoading) {
-    return <div>Initializing...</div>;
-  }
-
-  if (!isAuthenticated) {
-    return <button onClick={onSignIn}>Signin with Google</button>;
+  if (googleApi.isLoading) {
+    return (
+      <Flex
+        pos="absolute"
+        top={0}
+        left={0}
+        align="center"
+        justify="center"
+        w="100%"
+        h="100%"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="200"
+          viewBox="0 0 200 200"
+        >
+          <path
+            fill="#F0EB70"
+            d="M73 50c0-12.7-10.3-23-23-23S27 37.3 27 50m3.9 0c0-10.5 8.5-19.1 19.1-19.1S69.1 39.5 69.1 50"
+          >
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="rotate"
+              dur="1s"
+              from="0 50 50"
+              to="360 50 50"
+              repeatCount="indefinite"
+            />
+          </path>
+        </svg>
+      </Flex>
+    );
   }
 
   return (
-    <div className="App">
-      <h2>Email: {currentUser && currentUser.email}</h2>
-      <h2>Google ID: {currentUser && currentUser.googleId}</h2>
-      <button onClick={onSignOut}>Sign out</button>
-      <Route exact path="/">
-        <Splash auth={{ onSignIn, onSignOut }} />
-      </Route>
+    <Stack pos="relative" w="100%" minHeight="100vh">
+      <Header />
+
       <Route path="/authenticate/google">
         <Authenticate />
       </Route>
-
-      <PrivateRoute path="/home">
-        <Home profile={currentUser} eventsApi={eventsApi} />
-      </PrivateRoute>
-
-      <Route path="/events">
-        <Events eventsApi={eventsApi} />
+      <Route exact path="/">
+        <Welcome />
       </Route>
-
-      {/* <PrivateRoute path="/events">
-        <Events eventsApi={eventsApi} />
-      </PrivateRoute> */}
-    </div>
+      <PrivateRoute path="/:id/dashboard">
+        <Dashboard />
+      </PrivateRoute>
+    </Stack>
   );
 }
 
