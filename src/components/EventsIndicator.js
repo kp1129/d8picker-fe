8b534c12c924868d8cfb8036c34365a51bca1eb3
Alexander Sierra <alexandercsierra@gmail.com>
2020-03-17 19:19:14 -0400

    altered hasEvents to account for events without datetime

--- src/components/EventsIndicator.js
+++ src/components/EventsIndicator.js
@@ -5,10 +5,24 @@ import { Icon } from '@chakra-ui/core';
 
 // Displays circle icon if day has an event
 const EventIndicator = ({ event, day, currentMonth, currentYear }) => {
-  const hasEvents =
+  // const hasEvents =
+  //   day === dayjs(event.start.dateTime).date() &&
+  //   currentMonth === dayjs(event.start.dateTime).month() &&
+  //   currentYear === dayjs(event.start.dateTime).year();
+
+  let hasEvents;
+  if(event.start.dateTime){
+    hasEvents =
     day === dayjs(event.start.dateTime).date() &&
     currentMonth === dayjs(event.start.dateTime).month() &&
     currentYear === dayjs(event.start.dateTime).year();
+  } else if (event.start.date){
+    hasEvents =
+    day === dayjs(event.start.date).date() &&
+    currentMonth === dayjs(event.start.date).month() &&
+    currentYear === dayjs(event.start.date).year();
+  }
+  
 
   return hasEvents ? (
     <>
