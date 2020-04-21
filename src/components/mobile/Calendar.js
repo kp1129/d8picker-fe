1286607ac308c61d1e6138967e8c837965fe4bba
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-04-21 01:57:31 -0400

    infinite list rendering numbers

--- src/components/mobile/Calendar.js
+++ src/components/mobile/Calendar.js
@@ -7,19 +7,17 @@ import Cell from '../dashboardComponents/calendarComponents/Cell';
 import useDate from '../../hooks/useDate';
 import styled from 'styled-components';
 
-//for some reason, a regular useEffect doesn't work but putting this out here like this does
-const useMountEffect = (fun, numOfMonths) => useEffect(fun, [numOfMonths])
 
-const Calendar = ({ events, selected, setSelected, templateFormOpen, month,  monthList, setMonths, numOfMonths, setNumOfMonths, i, reloadMonths, setReloadMonths, setStartMonth, startMonth, visibleMonths, setVisibleMonths, ref}) => {
 
-  console.log('calref', ref)
+const Calendar = ({ events, selected, setSelected, templateFormOpen, month,  monthList, i}) => {
+
+ 
 
   const currentDay = dayjs();
   // state to display cuurent date
   const [date, setDate] = useState(dayjs());
 
-  //define a month when user scrolls to it will trigger load of additional months (in this case somewhere in the middle of the year)
-  const [reloadMonth, setReloadMonth] = useState(monthList[monthList.length-6].format('MMMM'))
+
 
 
 
@@ -31,78 +29,14 @@ const Calendar = ({ events, selected, setSelected, templateFormOpen, month,  mon
     weekDayOfLastDoM,
     weekDays
   } = useDate(date);
-
-  console.log('num of months from calendar body', numOfMonths)
- 
-  const scrollToRef = (ref) => {
-    // window.addEventListener('scroll', ()=>{
-
-       if(ref !==null){
-         //needs a setTimeout because ref and virtual dom don't always agree if ref exists or not
-        setTimeout(()=>{
-          
-        let reloadDay = monthList[monthList.length-6].$d
-        // console.log('dimensions', ref.current.getBoundingClientRect().height)
-        
-        //if the user scrolls past a month whose name matches the reloadMonth, load more months
-        
-        //grabs the year of the current ref from it's text and converts to a number for comparison
-        let yearIndex = ref.current.innerText.match(/[0-9][0-9][0-9]|[0-9]/).index;
-        let yearText = ref.current.innerText.substring(yearIndex, yearIndex+4);
-        let yearNum = Number(yearText)
-        console.log('scrollY', window.scrollY, 'ref.current', ref.current.offsetTop)
-        //checks what month we've scrolled past and if the month and year names match our reloadMonth
-        if (window.scrollY > ref.current.offsetTop && ref.current.innerText.substring(0,3) === reloadMonth.substring(0,3) && yearNum === month.$y){
-            //if yes, load more months
-            console.log('should load more months')
-            setNumOfMonths(numOfMonths + 12)
-            
-            //check dimensions of calendars above and subtract when changing start and end
-
-
-            // setNumOfMonths(numOfMonths + 12)
-            // setStartMonth(startMonth + 12)
-
-            // window.scrollTo(0, 0);
-
-
-
-
-
-
-            // console.log('num of months before', numOfMonths)
-            // if(!reloadMonths.includes(reloadDay)){
-            //   setNumOfMonths(numOfMonths + 12)
-            //   setReloadMonths([...reloadMonths, reloadDay])
-            //   // setVisibleMonths({start: numOfMonths, end: numOfMonths+12})
-            // }
-                // setStartMonth(startMonth + 12)
-              
-
-        }
-
-        }, 350)
-       } else {
-         console.log("LOADING")
-       }
-    // })
-    // return window.removeEventListener('scroll', ()=>console.log('removed'))
-    }
-  
   
 
   useEffect(()=>{
     setDate(month)
   },[monthList])
 
-  const myRef = useRef("")
-  // useMountEffect(() => scrollToRef(myRef),[numOfMonths])
-  
-  
-
-  // if(i >= visibleMonths.start && i < visibleMonths.end){
     return (
-      <Box ref={myRef} id={i} className="calendar" backgroundColor="white" borderRadius="10px">
+      <Box id={i} className="calendar" backgroundColor="white" borderRadius="10px">
         <Flex className="header" align="center" justify="center" py={4}>
           <MonthNameContainer>
             {/* <Heading className="heading">{date.format('MMMM')} {date.format('YYYY')}</Heading> */}
@@ -148,13 +82,6 @@ const Calendar = ({ events, selected, setSelected, templateFormOpen, month,  mon
         </Grid>
       </Box>
     );
-  // } else {
-  //   return(
-  //     <div style={{height: "817px"}} ref={myRef}>
-  //        <Heading className="heading">{month.format('MMMM')} {date.format('YYYY')}</Heading>
-  //     </div>
-  //   )
-  // }
 };
 
 export default Calendar;
--- src/components/mobile/Dashboard.js
+++ src/components/mobile/Dashboard.js
@@ -5,11 +5,10 @@ import { useAuth } from '../../contexts/auth';
 import Calendar from './Calendar';
 import dayjs from 'dayjs';
 import { FixedSizeList as List } from 'react-window';
+import InfiniteLoader from "react-window-infinite-loader";
 import AltCalendar from './AltCalendar'
-import Welcome from '../Welcome'
 
-const useMountEffect = (fun, numOfMonths) => useEffect(fun, [numOfMonths])
-
+//gets event templates from backend
 const getTemplateList = async ({ googleId }) => {
   try {
     const response = await axios.get(
@@ -21,25 +20,35 @@ const getTemplateList = async ({ googleId }) => {
   }
 };
 
-const Row = ({index, style}) => (
-  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
-    <Welcome/>
-  </div>
-);
-const scrollToRef = (ref) => {console.log('theref', ref)}
-
 const Dashboard = ({ setUserState }) => {
+
+  //google OAuth2
   const { googleApi, api } = useAuth();
+  const { currentUser, handleSignOut } = googleApi;
 
   const [templateList, setTemplateList] = useState([]);
   const [templateFormOpen, setTemplateFormOpen] = useState(false);
+  
+  // state to show users events
+  const [events, setEvents] = useState(null);
+
   const [formOpen, setFormOpen] = useState(false);
+  //dates selected to add template to
   const [selected, setSelected] = useState([]);
+  //shadow to indicate select date mode is enabled
   const [shadow, setShadow] = useState("");
-  const { currentUser, handleSignOut } = googleApi;
+  
+  //sets initial number of months to display
+  const [numOfMonths, setNumOfMonths] = useState(8);
+
+  //array which will hold all of the months on the DOM
+  const [months, setMonths] = useState([])
 
   
 
+
+
+  //gets event templates assigned to user from backend
   useEffect(() => {
     (async () => {
       const templates = await getTemplateList(currentUser);
@@ -56,18 +65,14 @@ const Dashboard = ({ setUserState }) => {
     }
   }, [templateFormOpen])
   
-  //end months
-  const [numOfMonths, setNumOfMonths] = useState(8);
-  //start of months
-  const [startMonth, setStartMonth] = useState(-1);
-  const [months, setMonths] = useState([])
-  const [reloadMonths, setReloadMonths] = useState([])
+
+  //dynamically sets the state of months based on the state numOfMonths
   useEffect(()=>{
     setMonths(nextMonth(numOfMonths));
     console.log('months', nextMonth(numOfMonths))
   },[numOfMonths])
   
-
+  //helper function to loop create months in the future based on numOfMonths
   const nextMonth = (numOfMonths) => {
     let arr = [];
     for(let i=0; i<numOfMonths; i++){
@@ -76,11 +81,7 @@ const Dashboard = ({ setUserState }) => {
     return arr;
   }
 
-  const calRef = useRef("")
-  useMountEffect(() => scrollToRef(calRef),[numOfMonths])
 
-  // state to show users events
-  const [events, setEvents] = useState(null);
 
   // get events from api and set to state
   useEffect(() => {
@@ -94,16 +95,22 @@ const Dashboard = ({ setUserState }) => {
     })();
   }, [api]);
 
-  const[visibleMonths, setVisibleMonths] = useState({start: 0, end: 12})
 
-  const [num, setNum] = useState(5);
-  
+ //infinite loading stuff
+const [items, setItems] = useState(nextMonth(2));
+const [moreItemsLoading, setMoreItemsLoading] = useState(false);
+const [hasNextPage, setHasNextPage] = useState(true);
 
-  useEffect(()=>{
-    setTimeout((num)=>{num+=2},5000)
-    console.log('num', num)
-  },[num])
-  const listRef = useRef("")
+useEffect(()=>{
+  console.log('items has changed')
+},[items])
+
+const loadMore = () => {
+  console.log('loading more');
+  setItems([...items, nextMonth(numOfMonths+ 12)])
+}
+
+ //end infinite loading stuff
 
   
   return (
@@ -120,70 +127,50 @@ const Dashboard = ({ setUserState }) => {
         gridTemplateAreas={["'sidebar' 'main'", "'sidebar main'"]}
       >
         <Box className="calendarArea" gridArea="main" style={{ boxShadow: shadow }}>
-          {/* {months.map((thisMonth, i)=>{
-            return <Calendar 
-            key={i}
-            api={api}
-            i={i}
-            selected={selected}
-            setSelected={setSelected}
-            templateFormOpen={templateFormOpen}
-            setTemplateFormOpen={setTemplateFormOpen}
-            events={events}
-            month={thisMonth}
-            monthList={months}
-            setMonths={setMonths}
-            numOfMonths={numOfMonths}
-            setNumOfMonths={setNumOfMonths}
-            setStartMonth={setStartMonth}
-            startMonth={startMonth}
-            reloadMonths={reloadMonths}
-            setReloadMonths={setReloadMonths}
-            visibleMonths={visibleMonths}
-            setVisibleMonths={setVisibleMonths}
-          />
-          })} */}
-
-        {months.length > 0 && <List
-            ref={listRef}
-            className="List"
-            height={window.innerHeight}
-            itemCount={numOfMonths}
-            itemSize={1000}
-            width={window.innerWidth}
-            months={months}
-          >
-            {({index, style}) => {
-              return  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
-              {/* <Welcome/> */}
-              {/* {console.log('months inside row', months)} */}
-              {/* {console.log('listref', listRef.current)} */}
-              {console.log('numofmonths', numOfMonths)}
-              <Calendar 
-            ref={calRef}  
-            key={index}
-            api={api}
-            i={index}
-            selected={selected}
-            setSelected={setSelected}
-            templateFormOpen={templateFormOpen}
-            setTemplateFormOpen={setTemplateFormOpen}
-            events={events}
-            month={months[index]}
-            monthList={months}
-            setMonths={setMonths}
-            numOfMonths={numOfMonths}
-            setNumOfMonths={setNumOfMonths}
-            setStartMonth={setStartMonth}
-            startMonth={startMonth}
-            reloadMonths={reloadMonths}
-            setReloadMonths={setReloadMonths}
-            visibleMonths={visibleMonths}
-            setVisibleMonths={setVisibleMonths}
-          />
-            </div>
-            }}
-          </List>}
+        {months.length > 0 && 
+  //       <InfiniteLoader
+  //           isItemLoaded={index => index < numOfMonths}
+  //           itemCount={itemCount}
+  //           loadMoreItems={loadMore}
+  //       >
+  //         <List
+  //           className="List"
+  //           height={window.innerHeight}
+  //           itemCount={itemCount}
+  //           itemSize={1000}
+  //           width={window.innerWidth}
+  //           months={months}
+  //         >
+  //           {({index, style}) => {
+              
+  //             return  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
+  //             {console.log('numofmonths', numOfMonths)}
+  //             {/* <Calendar 
+  //               key={index}
+  //               api={api}
+  //               i={index}
+  //               selected={selected}
+  //               setSelected={setSelected}
+  //               templateFormOpen={templateFormOpen}
+  //               setTemplateFormOpen={setTemplateFormOpen}
+  //               events={events}
+  //               month={months[index]}
+  //               monthList={months}
+  //             /> */}
+  //             <AltCalendar/>
+  //           </div>
+  //           }}
+  //         </List>
+  //     )}
+  // </InfiniteLoader>
+        <AltCalendar
+          items={items}
+          moreItemsLoading={moreItemsLoading}
+          loadMore={loadMore}
+          hasNextPage={hasNextPage}
+        />
+        
+        }
           
         </Box>
       </Grid>
