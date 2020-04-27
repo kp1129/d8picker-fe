d6af43fb42271f7daf8b9745a677f1cbbcb703f8
Alexander Sierra <alexandercsierra@gmail.com>
2020-04-27 15:35:57 -0400

    fixed photo broken link

--- src/components/mobile/Hamburger/TopNav.js
+++ src/components/mobile/Hamburger/TopNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./TopNav.css"
 import { useAuth } from '../../../contexts/auth';
 import {Image} from '@chakra-ui/core';
@@ -8,6 +8,11 @@ const Hamburger = () => {
   const { googleApi} = useAuth();
   const { currentUser, handleSignOut } = googleApi;
   console.log("In hamburger")
+  const [img, setImg] = useState(currentUser.photoUrl);
+
+  useEffect(()=>{
+    setImg(currentUser.photoUrl)
+  },[])
 
   return (
     <div>
@@ -23,7 +28,7 @@ const Hamburger = () => {
         <Image
               rounded="full"
               size="45px"
-              src={currentUser.photoUrl}
+              src={img}
               alt="avatar"
               // mb={2}
               style={{marginTop: window.innerHeight*-.068, marginLeft: "2%", marginBottom: "2%"}}
