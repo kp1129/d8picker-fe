1813200ec0bc34a218339ba3d85433d7a014811f
Alexander Sierra <alexandercsierra@gmail.com>
2020-04-27 14:57:25 -0400

    added profile pic to top nav

--- src/components/mobile/Hamburger/TopNav.js
+++ src/components/mobile/Hamburger/TopNav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./TopNav.css"
 import { useAuth } from '../../../contexts/auth';
+import {Image} from '@chakra-ui/core';
+
 
 const Hamburger = () => {
   const { googleApi} = useAuth();
@@ -9,12 +11,23 @@ const Hamburger = () => {
 
   return (
     <div>
+      
       <input type="checkbox" className="blue" id="menu"/>
+      
         <label htmlFor="menu" className="icon">
+        
           <div className="menu"></div>
         </label>
-
+       
         <nav className = "burger-container">
+        <Image
+              rounded="full"
+              size="45px"
+              src={currentUser.photoUrl}
+              alt="avatar"
+              // mb={2}
+              style={{marginTop: window.innerHeight*-.068, marginLeft: "2%", marginBottom: "2%"}}
+            />
           <ul>
             <li onClick={() => {
               handleSignOut();
