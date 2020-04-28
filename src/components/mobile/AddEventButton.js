484ff67c218098d22a8380ccd9cdd5209aa02c3e
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-04-28 13:36:13 -0400

    changed add button to image, adjusted position

--- src/components/mobile/AddEventButton.js
+++ src/components/mobile/AddEventButton.js
@@ -1,35 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import btn from './NavigationComponents/NavImgs/addeventbtn.png'
 
 const AddEventButton = ({setNavState}) => {
-
-    let bottom = 100 - Math.floor(window.innerHeight * .099) 
-
-
-    const Btn = styled.div`
-    background: #28807D;
-    border-radius: 100%;
-    width: 46px;
-    height: 46px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 50px;
-    position: absolute;
-    bottom: ${bottom}%;
-    
-    left: 85%;
-    cursor: pointer;
-    &:hover{
-        background: #1f605d;
-    }
-    
-`;
-
-
-    return <Btn onClick={()=>setNavState(3)}>+</Btn>
+    return <Img src={btn} onClick={()=>setNavState(3)}/>
 }
 
 export default AddEventButton;
+
+
+
+    const Img = styled.img`
+    position: fixed;
+    bottom: 20%;
+    left: 80%; 
+    cursor: pointer;    
+    `;
 
