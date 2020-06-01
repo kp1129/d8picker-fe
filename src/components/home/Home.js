49ef11f0af0a2a7e42402b5a3e01913e0de2729c
vladmog <vladmog@gmail.com>
2020-06-01 13:21:05 -0700

    made mobile desktop transition dynamic

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,12 @@
-
-import React, {useState, useEffect} from 'react'
-import {Context} from '../../contexts/Contexts'
-import Dashboard from './Dashboard'
-import Events from '../events/Events'
-import Nav from '../navigation/Nav'
+import React, { useState, useEffect } from 'react';
+import { Context } from '../../contexts/Contexts';
+import Dashboard from './Dashboard';
+import Events from '../events/Events';
+import Nav from '../navigation/Nav';
 import NewEventForm from '../events/NewEventForm';
 import UpdateEventForm from '../events/UpdateEventForm';
-import Groups from '../groups/Groups'
-import axiosWithAuth from '../../utils/axiosWithAuth'
+import Groups from '../groups/Groups';
+import axiosWithAuth from '../../utils/axiosWithAuth';
 import { useAuth } from '../../contexts/auth';
 
 //gets list of templates from backend
@@ -23,51 +22,50 @@ const getTemplateList = async ({ googleId, token }) => {
 };
 
 const Home = () => {
-    // 0 = calendar, 1 = events, 2 = groups
-    const [navState, setNavState] = useState(0)
-    
-    //deals with toggling event selection mode
-    const [formOpen, setFormOpen] = useState(false);
-    const [templateFormOpen, setTemplateFormOpen] = useState(false);
+  // 0 = calendar, 1 = events, 2 = groups
+  const [navState, setNavState] = useState(0);
 
-    //list of event templates from backend
-    const [templateList, setTemplateList] = useState([]);
+  //deals with toggling event selection mode
+  const [formOpen, setFormOpen] = useState(false);
+  const [templateFormOpen, setTemplateFormOpen] = useState(false);
 
-    //holds the start and end time of currently selected event. 
-    const [conStart, setConStart] = useState("");
-    const [conEnd, setConEnd] = useState("");
+  //list of event templates from backend
+  const [templateList, setTemplateList] = useState([]);
 
-    //holds the current title (event name)
-    const [title, setTitle] = useState("")
+  //holds the start and end time of currently selected event.
+  const [conStart, setConStart] = useState('');
+  const [conEnd, setConEnd] = useState('');
 
-    //holds the current notes (event description)
-    const [notes, setNotes] = useState("")
+  //holds the current title (event name)
+  const [title, setTitle] = useState('');
 
-    //toggles whether the nav is shown or not, also controls the confirm dates button
-    const [toggleNav, setToggleNav] = useState(true);
+  //holds the current notes (event description)
+  const [notes, setNotes] = useState('');
 
-    //holds dates selected before confirming and sending to calendar api
-    const [selected, setSelected] = useState([]);
+  //toggles whether the nav is shown or not, also controls the confirm dates button
+  const [toggleNav, setToggleNav] = useState(true);
 
-    //controls state of nav button colors
-    const [colors, setColors] = useState(["#999898", "#999898", "#999898"])
+  //holds dates selected before confirming and sending to calendar api
+  const [selected, setSelected] = useState([]);
 
-    // holds the id of the template to update
-    const [templateIdToUpdate, setTemplateIdToUpdate] = useState(null)
-    
+  //controls state of nav button colors
+  const [colors, setColors] = useState(['#999898', '#999898', '#999898']);
 
-    //changes the color of the nav icons depending on which components are rendered
-    useEffect(()=>{
-        let newColors = [...colors];
-        newColors[newColors.indexOf("#28807D")] = "#BDBDBD"
-        newColors[navState] = "#28807D";
-        setColors(newColors)
-    },[navState])
+  // holds the id of the template to update
+  const [templateIdToUpdate, setTemplateIdToUpdate] = useState(null);
+
+  //changes the color of the nav icons depending on which components are rendered
+  useEffect(() => {
+    let newColors = [...colors];
+    newColors[newColors.indexOf('#28807D')] = '#BDBDBD';
+    newColors[navState] = '#28807D';
+    setColors(newColors);
+  }, [navState]);
 
   //google OAuth2
   const { googleApi, api } = useAuth();
   const { currentUser } = googleApi;
-  
+
   //gets list of templates from backend when the user or date selection mode has changed, may be unnecessary given new organization of components
   useEffect(() => {
     (async () => {
@@ -76,31 +74,87 @@ const Home = () => {
     })();
   }, [currentUser, formOpen]);
 
-    return(
-        
-        <Context.Provider value={{formOpen, setFormOpen, setTemplateFormOpen, templateFormOpen, conStart, conEnd, title, notes, selected, setSelected, toggleNav, setToggleNav,setNavState, setConStart, setConEnd, setTitle, setNotes, setTemplateList, templateIdToUpdate, setTemplateIdToUpdate}}>
-            
-            {navState===0 && <Dashboard setTemplateList={setTemplateList}/>}
-        
-            {navState===1 && <>
-                <Events formOpen={formOpen} setTemplateList={setTemplateList} templateList={templateList} />
-            </>}
+  return (
+    <Context.Provider
+      value={{
+        formOpen,
+        setFormOpen,
+        setTemplateFormOpen,
+        templateFormOpen,
+        conStart,
+        conEnd,
+        title,
+        notes,
+        selected,
+        setSelected,
+        toggleNav,
+        setToggleNav,
+        setNavState,
+        setConStart,
+        setConEnd,
+        setTitle,
+        setNotes,
+        setTemplateList,
+        templateIdToUpdate,
+        setTemplateIdToUpdate
+      }}
+    >
+      {navState === 0 && <Dashboard setTemplateList={setTemplateList} />}
 
-            {navState===2 && <Groups/>}
+      {navState === 1 && (
+        <>
+          <Events
+            formOpen={formOpen}
+            setTemplateList={setTemplateList}
+            templateList={templateList}
+          />
+        </>
+      )}
 
+      {navState === 2 && <Groups />}
 
-            {navState===3 && <NewEventForm setTemplateList={setTemplateList} templateList={templateList} setToggleNav={setToggleNav} setNavState={setNavState} setTitle={setTitle} setNotes={setNotes} setConStart={setConStart} setConEnd={setConEnd} setTemplateFormOpen={setTemplateFormOpen} setFormOpen={setFormOpen}/>}
+      {navState === 3 && (
+        <NewEventForm
+          setTemplateList={setTemplateList}
+          templateList={templateList}
+          setToggleNav={setToggleNav}
+          setNavState={setNavState}
+          setTitle={setTitle}
+          setNotes={setNotes}
+          setConStart={setConStart}
+          setConEnd={setConEnd}
+          setTemplateFormOpen={setTemplateFormOpen}
+          setFormOpen={setFormOpen}
+        />
+      )}
 
-            {navState===4 && <UpdateEventForm setTemplateList={setTemplateList} templateList={templateList} setToggleNav={setToggleNav} setNavState={setNavState} setTitle={setTitle} setConStart={setConStart} setConEnd={setConEnd} setTemplateFormOpen={setTemplateFormOpen} setFormOpen={setFormOpen}  />}
+      {navState === 4 && (
+        <UpdateEventForm
+          setTemplateList={setTemplateList}
+          templateList={templateList}
+          setToggleNav={setToggleNav}
+          setNavState={setNavState}
+          setTitle={setTitle}
+          setConStart={setConStart}
+          setConEnd={setConEnd}
+          setTemplateFormOpen={setTemplateFormOpen}
+          setFormOpen={setFormOpen}
+        />
+      )}
 
-            {toggleNav && <Nav navState={navState} setNavState={setNavState} colors={colors} setTemplateFormOpen={setTemplateFormOpen} setFormOpen={setFormOpen} setSelected={setSelected} setToggleNav={setToggleNav}/>}
-        </Context.Provider>
-    )
+      {toggleNav && (
+        <Nav
+          navState={navState}
+          setNavState={setNavState}
+          colors={colors}
+          setTemplateFormOpen={setTemplateFormOpen}
+          setFormOpen={setFormOpen}
+          setSelected={setSelected}
+          setToggleNav={setToggleNav}
+        />
+      )}
+    </Context.Provider>
+  );
+};
 
-
-    
-}
-
-export default Home
-
-
+export default Home;
--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -7,6 +7,7 @@ import settingsBtnActive from '../navigation/NavImgs/Settings Button-Active.png'
 import settingsBtnInactive from '../navigation/NavImgs/Settings Button-Inactive.png';
 import groupsBtnInactive from '../navigation/NavImgs/Group Button-Inactive.png';
 import eventsBtnInactive from '../navigation/NavImgs/Events Button-Inactive.png';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 const Nav = ({
   setNavState,
@@ -18,9 +19,9 @@ const Nav = ({
 }) => {
   const [navToggle, setNavToggle] = useState(false);
   const [visible, setVisible] = useState(false);
-    // this controls the add event icon 
+  // this controls the add event icon
   const [hidden, setHidden] = useState(true);
-   // this controls the arrow toggles in desktop
+  // this controls the arrow toggles in desktop
   const [calendarIcon, setCalendarIcon] = useState(false);
   const [eventsIcon, setEventsIcon] = useState(false);
   const [groupsIcon, setGroupsIcon] = useState(false);
@@ -30,7 +31,7 @@ const Nav = ({
   const [isDisplayingGroups, setIsDisplayingGroups] = useState(false);
   const [isDisplayingCalendar, setIsDisplayingCalendar] = useState(false);
   const [isDisplayingSettings, setIsDisplayingSettings] = useState(false);
-
+  const { height, width } = useWindowDimensions();
 
   //closes date selection more when a navigation to a new page, and empties date selection
   const handleChange = num => {
@@ -42,49 +43,74 @@ const Nav = ({
     setHidden(false);
   };
 
-    const handleCalendar = () => {
-        setIsDisplayingCalendar(!isDisplayingCalendar);
-    };
-    const handleEvents = () => {
-        setIsDisplayingEvents(!isDisplayingEvents);
-    };
-    const handleGroups = () => {
-      setIsDisplayingGroups(!isDisplayingGroups);
-    };
-    const handleSettings = () => {
-      setIsDisplayingSettings(!isDisplayingSettings);
-    };
-
+  const handleCalendar = () => {
+    setIsDisplayingCalendar(!isDisplayingCalendar);
+  };
+  const handleEvents = () => {
+    setIsDisplayingEvents(!isDisplayingEvents);
+  };
+  const handleGroups = () => {
+    setIsDisplayingGroups(!isDisplayingGroups);
+  };
+  const handleSettings = () => {
+    setIsDisplayingSettings(!isDisplayingSettings);
+  };
 
   //icon and label colors change based on navState
   return (
     <Container>
       <NavContainer>
-        <IconDiv className='calendarIcon' onClick={handleCalendar}>
+        <div>
+          height: {height}, width: {width}
+        </div>
+        <IconDiv className="calendarIcon" onClick={handleCalendar}>
           <Img src={calendarBtnInactive} />
           <Label style={{ color: colors[2] }}>Calendar</Label>
-          <Arrow className={isDisplayingCalendar ? "fas fa-chevron-down" : "fas fa-chevron-up"}></Arrow>
+          <Arrow
+            className={
+              isDisplayingCalendar ? 'fas fa-chevron-down' : 'fas fa-chevron-up'
+            }
+          ></Arrow>
         </IconDiv>
-        <IconDiv className='eventsIcon' onClick={handleEvents}>
-          <Img src={eventsBtnInactive} style={{ fontSize: '2rem', color: colors[0] }} />
+        <IconDiv className="eventsIcon" onClick={handleEvents}>
+          <Img
+            src={eventsBtnInactive}
+            style={{ fontSize: '2rem', color: colors[0] }}
+          />
           <Label style={{ color: colors[1] }}>Events</Label>
-          <Arrow className={isDisplayingEvents ? "fas fa-chevron-down" : "fas fa-chevron-up"}></Arrow>
+          <Arrow
+            className={
+              isDisplayingEvents ? 'fas fa-chevron-down' : 'fas fa-chevron-up'
+            }
+          ></Arrow>
         </IconDiv>
-        <IconDiv className='groupIcon' onClick={handleGroups}>
-            <Img src={groupsBtnInactive} style={{ fontSize: '2rem', color: colors[0] }} />
+        <IconDiv className="groupIcon" onClick={handleGroups}>
+          <Img
+            src={groupsBtnInactive}
+            style={{ fontSize: '2rem', color: colors[0] }}
+          />
           <Label style={{ color: colors[2] }}>Groups</Label>
-          <Arrow className={isDisplayingGroups ? "fas fa-chevron-down" : "fas fa-chevron-up"}></Arrow>
+          <Arrow
+            className={
+              isDisplayingGroups ? 'fas fa-chevron-down' : 'fas fa-chevron-up'
+            }
+          ></Arrow>
         </IconDiv>
-        <IconDiv className='settingsIcon' onClick={handleSettings}>
-            <Img src={settingsBtnInactive} />
+        <IconDiv className="settingsIcon" onClick={handleSettings}>
+          <Img src={settingsBtnInactive} />
 
           <Label style={{ color: colors[2] }}>Settings</Label>
-          <Arrow className={isDisplayingSettings ? "fas fa-chevron-down" : "fas fa-chevron-up"}></Arrow>
+          <Arrow
+            className={
+              isDisplayingSettings ? 'fas fa-chevron-down' : 'fas fa-chevron-up'
+            }
+          ></Arrow>
         </IconDiv>
-        <IconDiv className={isDisplayingEvents ? "addEventIcon" : "eventBtn"}>
-            <AddEventButton />
-          <Label style={{ color: colors[2], marginTop: '6px'}}>Add Event</Label>
-
+        <IconDiv className={isDisplayingEvents ? 'addEventIcon' : 'eventBtn'}>
+          <AddEventButton />
+          <Label style={{ color: colors[2], marginTop: '6px' }}>
+            Add Event
+          </Label>
         </IconDiv>
       </NavContainer>
       {navToggle === true && <Hamburger />}
@@ -93,12 +119,12 @@ const Nav = ({
 };
 
 const size = {
-    tablet: '768px',
-    desktop: '1024px'
-}
+  tablet: '768px',
+  desktop: '1024px'
+};
 const device = {
-    desktop: `(min-width: ${size.desktop})`
-}
+  desktop: `(min-width: ${size.desktop})`
+};
 
 const NavContainer = styled.div`
   width: 100%;
@@ -108,18 +134,18 @@ const NavContainer = styled.div`
   border-top: 1px solid #f2f2f2;
   background: white;
 
-    @media ${device.desktop} {
-        display: flex;
-        flex-direction: column;
-        align-items: center; 
-        align-content: stretch;
-        height: 80%;
-        padding-top: 15vh;
+  @media ${device.desktop} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: stretch;
+    height: 80%;
+    padding-top: 15vh;
 
-        .eventBtn {
-            display: none !important;
-        }
+    .eventBtn {
+      display: none !important;
     }
+  }
 `;
 const Container = styled.div`
   display: flex;
@@ -130,30 +156,30 @@ const Container = styled.div`
   border-top: 1px solid #f2f2f2;
   background: white;
 
-    @media ${device.desktop} {
-        position: fixed; 
-        border-top: 1px solid #f2f2f2;
-        background: white;
-        width: 18%;
-        height: 100%;
-        left: 0;
+  @media ${device.desktop} {
+    position: fixed;
+    border-top: 1px solid #f2f2f2;
+    background: white;
+    width: 18%;
+    height: 100%;
+    left: 0;
 
-        .eventsIcon {
-            order: 1 
-        }
-        .groupIcon {
-            order: 3
-        }
-        .calendarIcon {
-            order: 4
-        }
-        .settingsIcon {
-            order: 5
-        }
-        .addEventIcon {
-            order: 2
-        }
+    .eventsIcon {
+      order: 1;
     }
+    .groupIcon {
+      order: 3;
+    }
+    .calendarIcon {
+      order: 4;
+    }
+    .settingsIcon {
+      order: 5;
+    }
+    .addEventIcon {
+      order: 2;
+    }
+  }
 `;
 const IconDiv = styled.div`
   width: 61px;
@@ -163,46 +189,44 @@ const IconDiv = styled.div`
   justify-content: center;
   align-items: center;
 
-    @media ${device.desktop} {
-        flex-direction: row;
-        justify-content: space-around;
+  @media ${device.desktop} {
+    flex-direction: row;
+    justify-content: space-around;
 
-            .addEventText {
-                margin-left: 20px;
-            }
-            .groupsText {
-                padding: 
-            }
-            
+    .addEventText {
+      margin-left: 20px;
     }
+    .groupsText {
+      padding: ;
+    }
+  }
 `;
 const Label = styled.p`
   font-size: 14px;
   font-family: Open Sans;
   color: gray;
-    
-    @media ${device.desktop} {
-        font-size: 18px;
-    }
+
+  @media ${device.desktop} {
+    font-size: 18px;
+  }
 `;
 
 const Img = styled.img`
-margin-bottom: 10px;
+  margin-bottom: 10px;
 
-    @media ${device.desktop} {
-        margin-right: 20px;
-    }
+  @media ${device.desktop} {
+    margin-right: 20px;
+  }
 `;
 
 const Arrow = styled.i`
-    display: none;
+  display: none;
 
-    @media ${device.desktop} {
-        display: block !important;
-        color: gray;
-        font-size: 1.4rem;
-        margin-left: 85%;
-    }
-
-`
+  @media ${device.desktop} {
+    display: block !important;
+    color: gray;
+    font-size: 1.4rem;
+    margin-left: 85%;
+  }
+`;
 export default Nav;
