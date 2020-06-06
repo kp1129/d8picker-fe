70fbe1d4b3f27abcd1ff87e595decd93c8e9c049
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-06-06 11:45:13 -0400

    hooked up post request to create a group to back-end

--- src/components/groups/CreateNewGroup.js
+++ src/components/groups/CreateNewGroup.js
@@ -1,14 +1,90 @@
 import React, { useState, useContext } from 'react';
 import { useAuth } from '../../contexts/auth';
-import { DashboardContext } from '../../contexts/Contexts'
 import styled from 'styled-components';
+import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import useGapi from '../../hooks/useGapi';
 // import { useToasts } from 'react-toast-notifications';
 
 const CreateNewGroup = ({setNavState}) => {
 
+    const { googleApi } = useAuth();
+    const { currentUser } = googleApi;
+    const { token } = currentUser;
+
+    // console.log('googleApi: ', googleApi)
+    // console.log('currentUser: ', currentUser)
+
+    const [newGroup, setNewGroup] = useState({
+        groupName: '',
+        groupDescription: '',
+    });
+
+    const [message, setMessage] = useState('')
+
+    const handleChange = e => {
+        setNewGroup({
+            ...newGroup,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(!newGroup.groupName){
+            (setMessage('Please provide a name for your group'))
+        }
+        axiosWithAuth(token)
+        .post('/api/admin', currentUser)
+        .then(res => {
+            console.log(res.data);
+            setNewGroup({
+                ...newGroup,
+                adminId: res.data.adminId
+            })
+            console.log('newGroup: ', newGroup)
+            axiosWithAuth(token)
+            .post('/api/groups', newGroup)
+            .then(res => {
+                console.log('RESPONSE: ', res)
+            })
+            .catch(err => {
+                console.log('ERROR 2: ', err)
+            })
+        })
+        .catch(err => {
+            console.log('ERROR 1: ', err)
+        })
+    }
+
     return (
         <Container>
-             <Cancel onClick={() => {setNavState(2)}}>Cancel</Cancel>
+            <Cancel onClick={() => {setNavState(2)}}>Cancel</Cancel>
+            <Header>New Group</Header>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="groupName"> Group Name: 
+                    <Input
+                    type="text"
+                    placeholder="New Group Name"
+                    name="groupName"
+                    value={newGroup.groupName}
+                    onChange={handleChange}
+                    />
+                </label>
+
+                <br/>
+
+                <label htmlFor="groupName"> Group Description: 
+                    <InputTextArea
+                    type="text"
+                    placeholder="New Group Description"
+                    name="groupDescription"
+                    value={newGroup.groupDescription}
+                    onChange={handleChange}
+                    />
+                </label>
+                <button type="submit" label="submit">Submit</button>
+            </form>
+            <p>{message}</p>
         </Container>
     )
 }
@@ -18,13 +94,26 @@ export default CreateNewGroup
 const Container = styled.div`
     width: 100%;
     display: flex;
+    flex-wrap: wrap;
+`
+
+const Header = styled.h1`
+    width: 60%;
+    font-size: 22px;
 `
 
 const Cancel = styled.p`
     width: 40%;
-    padding: 2% 0 0 2%;
     font-size: 20px;
     line-height: 27px;
     color: #28807D;
 
-`;
+`
+
+const Input = styled.input`
+    width: 100%;
+`
+
+const InputTextArea = styled.textarea`
+    width: 100%;
+`
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosWithAuth = (token) => {
-  console.log(token)
+  // console.log(token)
   return axios.create({
     baseURL: process.env.REACT_APP_ENDPOINT_URL,
     headers: {
